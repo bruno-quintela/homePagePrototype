3325d5b09f42eb5b21e8663028ba997ce7794f4c
bruno-quintela <bruno_quintela@hotmail.com>
2014-02-21 17:51:02 +0000

    cliping testes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-$(".border, #content").hide();
     $("#topbar").show();
-    $("#topbar").animate({width: "318px"},1000, function() {
-      $("#rightbar").show();
+    $("#rightbar").show();
+    $("#leftbar").show();
+    $("#bottombar").show();
+
+    $("#topbar").animate({width: "318px"},2000)
+    $("#rightbar").animate({height: "238px"},2000)
+    $("#bottombar").animate({width: "318px"},2000)
+    $("#leftbar").animate({height: "238px"},2000)
+
+    /*$("#rightbar").show();
       $("#rightbar").animate({height: "238px"},1000, function() {
-        $("#bottombar").show();
+       
         $("#bottombar").animate({width: "318px"},1000, function() {
-          $("#leftbar").show();
+   
           $("#leftbar").animate({height: "238px"},1000, function() {
             //$("#content").fadeIn(1000);
               $("#topbar").animate({ "border-width":"20px"},2000);
@@ -14,5 +21,4 @@ $(".border, #content").hide();
               $("#leftbar").animate({ "border-width":"20px"},2000);
           });
         });
-      });
-    });
+    });*/
