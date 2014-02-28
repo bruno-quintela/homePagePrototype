4858b51569638b5ed9180b061868d511afe22fc8
bruno-quintela <bruno_quintela@hotmail.com>
2014-02-28 16:32:37 +0000

    rotate added

--- js/phoneSlideshow.js
+++ js/phoneSlideshow.js
@@ -11,11 +11,20 @@
 var phoneSlideshow = (function() {
 
     var open = false;
+    var defaultRotation = -40;
 	function init() {
         
         $("#toggle").click(function(){
            changePerspective()
            open = !open;
+        })
+        
+        $("#rotateRight").click(function(){
+           rotatePerspective(90)
+        })
+        
+        $("#rotateLeft").click(function(){
+           rotatePerspective(-90)
         })
         
         function changePerspective()
@@ -26,8 +35,16 @@ var phoneSlideshow = (function() {
             else
             {
                 $(".ms-perspective").removeClass('ms-view-layers');
+                $(".ms-device").css('-webkit-transform','');
             }
         }
+        
+        function rotatePerspective(angleY)
+        {
+            defaultRotation += angleY;
+            $(".ms-device").css('-webkit-transform','rotateY('+defaultRotation+'deg) rotateX(90deg) translateZ(-15px) translateZ(-97px)');
+        }
+        
 		/*[].slice.call( document.querySelectorAll( '.ms-wrapper' ) ).forEach( function( el, i ) {
 			var open = false;
 			el.querySelector( 'button' ).addEventListener( 'click', changeView, false );
